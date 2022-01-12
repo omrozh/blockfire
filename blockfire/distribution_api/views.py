81ddb9299f8fe3b5846485dc6b9c9eeb4d58536a
omrozh <53612475+omrozh@users.noreply.github.com>
2022-01-12 11:55:34 +0300

    project starts - repo used only for backup for now

--- blockfire/distribution_api/views.py
+++ blockfire/distribution_api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import os
 from pathlib import Path
+from ..receiving_api.models import File, FileRef
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,8 +16,11 @@ def distribute(req):
     data = file.read()
 
     info = [data[i:i + 1024] for i in range(0, len(data), 1024)]
-    print(len(info))
 
     file.close()
 
-    return HttpResponse("End")
+    file_db = File.objects.filter(id_name=distribution_file).first()
+    file_db.latest_index += 1
+    file_db.save()
+
+    return HttpResponse(info[file_db.latest_index-1])
