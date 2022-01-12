b805d10cf319fd990e794cf888b01e7805fb3d0f
omrozh <53612475+omrozh@users.noreply.github.com>
2022-01-12 11:48:59 +0300

    project starts - repo used only for backup for now

--- blockfire/distribution_api/views.py
+++ blockfire/distribution_api/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 import os
 from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def distribute(req):
-    files = os.listdir(os.path.join(BASE_DIR, 'files'))
+    distribution_file = os.listdir(os.path.join(BASE_DIR, 'files'))[0]
+    file_path = os.path.join(os.path.join(BASE_DIR, 'files'), distribution_file)
 
-    with open(os.path.join(BASE_DIR, 'files'), "rb").read() as data:
-        info = [data[i:i + 2] for i in range(0, len(data), 1024)]
+    file = open(file_path, "rb")
+
+    data = file.read()
+
+    info = [data[i:i + 1024] for i in range(0, len(data), 1024)]
+    print(len(info))
+
+    file.close()
+
+    return HttpResponse("End")
